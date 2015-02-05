6f617465086d603738f683ab9abc7908d8773586
rcdavis4 <rcdavis4@gmail.com>
2015-02-05 08:01:10 -0800

    adding functionality

--- js/app.js
+++ js/app.js
@@ -1,7 +1,34 @@
 $(document).ready(function() {
+
+  // click add button to create item list
   $('.add').click(function() {
-    $('.item-list').show();
-    $('.list').append('<li><span class="checked fa fa-check"></span>temp item<span class="delete fa fa-times"></span></li>');
+
+    // create variable to store value from input element
+    var item = $('#add-item').val();
+
+    // makes new list item
+    $('#list').append('<li>' + item + '<span class="delete close"></span></li>');
+
+    // resets input field's placeholder value
+    $('input').val('');
   })
+
+  // pressing enter will also add new list item
+  $(document).keydown(function(event) {
+    if(event.which == 13) { // enter key
+      $('.add').click(); // calls click function from above
+    }
+  });
+
+  // clicking item crosses out
+  $('#list').click(function() {
+    $('.item-list li').toggleClass('checked');
+  })
+
+
+  // clicking x removes item
+
+  // select and hold raises item, changes color to arrange
+
 
 }); // end of document.ready
